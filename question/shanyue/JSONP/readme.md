a57f9f18baae3b9689767bc8cdfd45d8f3c7f728
52LONG <814405253@qq.com>
2020-12-22 21:31:54 +0800

    book-phone

---
+++ question/shanyue/JSONP/readme.md
@@ -0,0 +1,11 @@
+# JSONP
+- npm init -y 编译json文件
+1.HTTP 协议
+    TCP/IP  HTTP 是上层 
+    Web Server  RestFul
+
+    JSON 资源 ，喜欢的运动员 ， HTTP提供服务?  R请求  Q响应
+    url http://127.0.0.1/players.json
+    动作 Get 请求
+    live-server 
+    json-server
---
+++ wxapp/book/miniprogram/pages/index/index.js
@@ -0,0 +1,28 @@
+//使用云端数据
+const db = wx.cloud.database();//连接云端数据库
+const phoneTable = db.collection("products");//
+Page({
+  data:{
+    phone_list:[]
+  },
+  onLoad(){
+    //加载数据
+    db
+      .collection("products")
+      .get({
+        success: res => {
+          // console.log(res);
+          this.setData({
+            phone_list:res.data
+          })
+        }
+      })
+    
+  },
+  viewItem(e){
+    var id = e.currentTaget.dataset.id;
+    wx.navigateTO({
+      url:'../phoneDetail/phoneDetail?id='+id
+    })
+  }
+})
