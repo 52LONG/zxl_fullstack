012917b752672e6a0e2c352c4e48f420731a562b
52LONG <814405253@qq.com>
2021-02-03 21:53:58 +0800

    echarts

---
+++ batjtmd/baiduapp/pages/index/index.js
@@ -0,0 +1,27 @@
+/**
+ * @file index.js
+ * @author swan
+ */
+const app = getApp()
+
+Page({
+  data:{
+      charts:[{
+          id:'bar',
+          name:'柱状图'
+      },{
+          id:'pie',
+          name:'饼图'
+      },{
+          id:'line',
+          name:'折线图'
+      }]
+  },
+  showChart(e){
+      const id = e.currentTarget.dataset.id;
+      swan.navigateTo({
+          url:'/pages/'+id+'/'+id
+      });
+  }
+
+})
