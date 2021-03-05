401de27f30ae9f1f359648a2bb7319dd87729dd1
52LONG <814405253@qq.com>
2021-03-05 21:43:13 +0800

    decorator

---
+++ interview/js/decorator/a.js
@@ -0,0 +1,34 @@
+// @annotation//用处很大 装饰器模式
+//  class MyClass{ //MyClass 里有annotated属性？ 没有
+//     constructor() {
+//         this.name = '沿街' //public
+//     }
+//  }
+//  //不需要这么做
+// //  MyClass.annotated = false; //static 属性
+
+// // 有几个类，都有共有的属性的话
+//  //复用装饰函数 新的构建类的方
+//  function annotation(target){
+//      target.annotated = true;
+//  }
+
+
+//decorator 好高级
+// defineProperty(obj,{})
+class Person {
+    @readonly //装饰的不是一个类 装饰的是一个方法 方法可以得到readonly 能力 只可读 不可写
+    name(){
+        return `${this.first} ${this.last}`
+    }
+}
+//target class  方法，属性 
+// name  name 
+// descriptor   defineProperty
+function readonly(target, name, descriptor) {
+  //console.log(target, name, descriptor)
+  descriptor.writable = false;
+  descriptor.enumerable = true;
+  return descriptor;
+
+}
---
+++ interview/js/decorator/readme.md
@@ -0,0 +1,22 @@
+京东考题 
+ ES7
+ - @annotation
+  buffeer node 
+  html ,
+
+  babel + es6 比较新的特性题目
+  装饰器
+  装饰一下
+
+
+  浏览器  不支持怎么办？
+   babel presets 也是js 编译的预处理插件包 只不过是它把常用的core react 封装成env 或 jsx
+
+   如果有些特殊的 decorator async await 未来的特性  babel 使用特定的插件来增加编译
+   babel 永远可以让刚上市的最新的js特性直接使用起来
+   
+npm init -y
+npm i @babel/cli @babel/core @babel/preset-env
+./node_modules/.bin/babel  a.js -o compiled.js
+npm i @babel/plugin-proposal-decorators
+
