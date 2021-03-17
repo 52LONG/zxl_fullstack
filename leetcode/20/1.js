2949ad786b3ac2cb0d2216373e4920c9cc08daf7
52LONG <814405253@qq.com>
2021-03-17 11:05:20 +0800

    Leetcode 20

--- leetcode/20/1.js
+++ leetcode/20/1.js
@@ -1,22 +1,24 @@
 var isValid = function (s) {
     let valid = true;
-    const stack = [];
+    const stack = [];//数组栈
     const mapper = {
       "{": "}",
       "[": "]",
       "(": ")",
     };
   
-    for (let i in s) {
+    for (let i in s) {//时间复杂度 O(n)
       const v = s[i];
       //indexOf() 方法可返回某个指定的字符串值在字符串中首次出现的位置。
       if (["(", "[", "{"].indexOf(v) > -1) {
         stack.push(v);
       } else {
-          // pop 返回删除的元素。
+        // pop 返回删除的元素。
         const peak = stack.pop();
+        // console.log( v );
+        console.log(mapper[peak]);
         if (v !== mapper[peak]) {
-          return false;
+          return false;  
         }
       }
     }
@@ -25,5 +27,6 @@ var isValid = function (s) {
   
     return valid;
   };
-  console.log(isValid('(){)'));
-  console.log(isValid('(){}'));
+  // console.log(isValid('(){)'));
+  // console.log(isValid('(){}'));
+  console.log(isValid('([{}])'));
