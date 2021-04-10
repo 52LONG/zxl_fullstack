4913a7c180e229f824e3f805137a7df73a85d932
52LONG <814405253@qq.com>
2021-04-10 19:38:40 +0800

    vue-shop

---
+++ vue/shop/README.md
@@ -0,0 +1,4 @@
+- yarn add flexible 或npm i flexible
+- npm i vant@next -S
+- npm install --save lib-flexible
+
---
+++ vue/shop/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {//数据源
+  
+  },
+  mutations: {//放更改状态的函数
+  
+  },
+  actions: {//方法
+    
+  },
+  modules: {
+  }
+})
